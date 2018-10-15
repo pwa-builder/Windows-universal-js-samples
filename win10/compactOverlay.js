76909faede03a46410c27be43dc951c4a30f4e31
Jeff Burtoft <boyofgreen@gmail.com>
2018-10-15 12:31:23 -0500

    Update compactOverlay.js

--- win10/compactOverlay.js
+++ win10/compactOverlay.js
@@ -1,13 +1,13 @@
 /**
  * @file
- * The CompactOverlay (Video Player)
+ * The CompactOverlay
  */
 
 /**
  * When an app window enters compact overlay mode it’ll be shown above other windows so it won’t get blocked. This allows users to continue to keep an eye on your app's content even when they are working with something else. The canonical example of an app taking advantage of this feature is a media player or a video chat app.
  * This snippet allows you to switch into compact overlay mode or return to the default mode.
  *
- * @alias Toggle Compact Overlay mode video player
+ * @alias Toggle Compact Overlay mode
  * @method toggleCompactOverlayMode
  * @param {boolean} [forceCompactOverlay=false] Force Compact Overlay mode.
  * @returns {Promise} Promise with new mode value (1=CompactOverlay | 0=Default).
