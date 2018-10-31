509cbc5fa7b55225f08e7bcd29010df76b8c9844
boyofgreen <boyofgreen@gmail.com>
2018-10-30 20:34:35 -0700

    ,

--- win10/msGraphAuth.js
+++ win10/msGraphAuth.js
@@ -11,10 +11,10 @@
  * @param {object} [userAgentApplication = ""] The userAgentApplication object will help you doing the authentication job And get the token to do Graph API calls
  * @param {object} [user = ""] User object return by MSAL lib
  * @param {object} [msalconfig = {
-            clientID: "54a0ef8a-3934-49f4-85c8-f8e32511fd93",
+            clientID: "123ABC-1234-1234-1234-SAMPLE-ID",
             redirectUri: location.origin
         }] Register your app there: https://apps.dev.microsoft.com/portal/register-app & add a web platform to get a Client ID If you already did, retrieve the Client ID from: https://apps.dev.microsoft.com/#/appList
- * @param {object} [graphAPIScopes = \["https://graph.microsoft.com/contacts.read", "https://graph.microsoft.com/user.read", "https://graph.microsoft.com/sites.readwrite.all"\]] Permissions you're requesting to do your future Graph API calls
+ * @param {object} [graphAPIScopes = /["https://graph.microsoft.com/contacts.read", "https://graph.microsoft.com/user.read", "https://graph.microsoft.com/sites.readwrite.all"/]] Permissions you're requesting to do your future Graph API calls
  * @see
  */
 
@@ -52,7 +52,7 @@
                 console.log("You can now do calls to Graph API starting from here.");
             }
         }
-        
+
         //example element to attache a login button to
         document.getElementById("Login").addEventListener("click", () => {
             // Call this code on the click event of your login button
